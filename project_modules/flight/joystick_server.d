3115df1fd5684e3447f04a817d7e5b247abcc5e5
Paul Lassen <paullassen@protonmail.com>
2022-01-07 00:22:34 -0800

    removed *.d

--- project_modules/flight/joystick_server.d
+++
@@ -1,4 +0,0 @@
-joystick_server.o: /tmp/preamble-64f893.pch \
-  /home/pala/quadcopter/project_modules/tstst/include/joystick_server.h
-
-/home/pala/quadcopter/project_modules/tstst/include/joystick_server.h:
--- project_modules/flight/main.d
+++
@@ -1,7 +0,0 @@
-main.o: /home/pala/quadcopter/project_modules/tstst/include/motor.h \
-  /home/pala/quadcopter/project_modules/tstst/include/joystick_server.h \
-  /home/pala/quadcopter/project_modules/js2mtr/src/main.c
-
-/home/pala/quadcopter/project_modules/tstst/include/joystick_server.h:
-
-/home/pala/quadcopter/project_modules/js2mtr/src/main.c:
